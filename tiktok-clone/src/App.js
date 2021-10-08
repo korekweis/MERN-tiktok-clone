bc900807cb7a4a174546e63455afbebd0c8ed63e
korekweis <korekweis@gmail.com>
2021-10-07 19:06:22 -0700

    created react app

---
+++ tiktok-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> MERN TIKTOK CLONE </h1>
+
+      {/* app container */}
+        {/* videos */}
+    </div>
+  );
+}
+
+export default App;
